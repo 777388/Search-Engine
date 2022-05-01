216458d10e8612350cdd55a342c61b35323ba52a
777388 <96343159+777388@users.noreply.github.com>
2022-04-30 20:04:51 -0700

    Update ipaddresses.py

--- ipaddresses.py
+++ ipaddresses.py
@@ -7,27 +7,32 @@ def lookup(addr):
          return socket.gethostbyaddr(addr)[0]
      except socket.herror:
          return None
+
 def threadhead(arg):
-	for j in range(arg):
-		for i in range(0, 255, 1):
-			for n in range (0, 255, 1):
-				for k in range (0, 255, 1):
-					for d in range (0, 255, 1):
-						h = str(str(i)+"."+str(n)+"."+str(k)+"."+str(d))
-						g = str("\r"+h+"\r")
-						f = "curl -s -I "+h+" >> trash.txt"
-						t = os.system(f)
-						p = "python3 Sublist3r/sublist3r.py -d "+str(lookup(h))+" >> "+str(lookup(h))+".txt"
-						q = "wsl cat "+str(lookup(h))+".txt | gau --threads 5 --o "+str(lookup(h))+".txt"
-						if lookup(h) != None: 
-							print(h+str(lookup(h)))
-							o = os.system(p)
-							m = os.system(q)
-						else:
-							print(g, end='', flush=True)
+	for i in range(arg, arg+1, 1):
+		for n in range (0, 255, 1):
+			for k in range (0, 255, 1):
+				for d in range (0, 255, 1):
+					h = str(str(i)+"."+str(n)+"."+str(k)+"."+str(d))
+					g = str("\r"+h+"\r")
+					f = "curl -s -I "+h+" >> trash"+str(i)+".txt"
+					t = os.system(f)
+					p = "python3 Sublist3r/sublist3r.py -d "+str(lookup(h))+" >> "+str(lookup(h))+".txt"
+					q = "wsl cat "+str(lookup(h))+".txt | gau --threads 5 --o "+str(lookup(h))+".txt"
+					if lookup(h) != None: 
+						print(h+str(lookup(h))+"\r\n")
+						o = os.system(p)
+						m = os.system(q)
+					else:
+						print(g, end='', flush=True)
 				
+
 if __name__ == "__main__":
-	thread = Thread(target = threadhead, args=(1, ))
-	thread.start()
-	thread.join()
+	threads = []
+	for x in range(0, 255, 1):
+		thread = Thread(target = threadhead, args=(x, ))
+		threads.append(thread)
+		thread.start()
+	for thread in threads:
+		thread.join()
 	print("finished")
